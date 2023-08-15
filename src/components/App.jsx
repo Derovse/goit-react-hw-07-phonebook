390197dcc8e287a24ee6eb7bdc51d7a33957e7ed
Derovse <sashamartunyk2004@gmail.com>
2023-08-16 01:27:36 +0300

    dd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+import { ContactsList } from './ContactsList/ContactsList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Wrap } from './App.styled';
+//
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectContacts,
+  selectisLoading,
+  selectError,
+  selectFilteredContacts,
+} from '../redux/selectors';
+import { fetchContacts, addContact } from '../redux/operations';
+import { useEffect } from 'react';
+//
+
+export default function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectisLoading);
+  const error = useSelector(selectError);
+  const filter = useSelector(selectFilteredContacts);
+
+  const newContact = name => {
+    contacts.find(
+      contact => contact.name.toLowerCase() === name.name.toLowerCase()
+    )
+      ? alert(`${name.name} is already in contacts`)
+      : dispatch(addContact(name));
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    newContact(values);
+    resetForm();
+  };
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrap>
+      <Section title={`Phonebook`}></Section>
+      <ContactForm handleSubmit={handleSubmit} />
+      <Section title={`Contacts`}>
+        <Filter />
+        {isLoading && <p>Loading contacts...</p>}
+        {error && <p>{error}</p>}
+        {contacts.length > 0 && <ContactsList contacts={filter} />}
+      </Section>
+    </Wrap>
   );
-};
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import { Label, FindInput } from './Filter.styled';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { filterChange } from '../../redux/filterSlice';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const onWrite = e => {
+    dispatch(filterChange(e.currentTarget.value));
+  };
+
+  return (
+    <Label>
+      Find contacts by name
+      <FindInput
+        type="text"
+        name="filter"
+        onChange={onWrite}
+        placeholder="Search contacts"
+      />
+    </Label>
+  );
+};
+
+Label.propTypes = {
+  filter: PropTypes.string,
+};
